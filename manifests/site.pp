ba086442c58dec3212b1c28443ac98f30578eb68
Kendall Park <kendall.park@cph.org>
2013-04-03 16:25:19 -0500

    Added puppet modules to Puppetfile and default install sites.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -85,4 +85,24 @@ node default {
     ensure => link,
     target => $boxen::config::repodir
   }
+
+  # CPH defaults
+  include mysql # TODO: custom root db setup, prolly need to fork puppet-mysql
+  include sysctl
+  include redis
+  include xquartz
+  include imagemagick
+  include phantomjs
+  include jumpcut
+  include imageoptim
+  include gitx
+  include git_tower
+  include firefox
+  include chrome
+  include caffeine
+  include postgresapp
+  include pow
+  include textmate
+  include sublime_text_2
+
 }
