64b2a528cfa743882a2e5c0cba7ce389413de210
Bob Lail <bob.lail@cph.org>
2015-05-19 16:26:12 -0500

    [skip] Dropped Ruby 2.1.1

--- manifests/site.pp
+++ manifests/site.pp
@@ -72,7 +72,6 @@ node default {
   ruby::version { '1.9.3': }
   ruby::version { '2.0.0': }
   ruby::version { '2.1.0': }
-  # ruby::version { '2.1.1': }
   ruby::version { '2.1.2': }
 
   # common, useful packages
